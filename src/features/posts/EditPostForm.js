2da67ea7de9b0f5323644a7bdff585810164746f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 14:41:11 +0300

    Imported selectAllUsers from usersSlice

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
+import { selectAllUsers } from '../users/usersSlice'
 import { selectPostById } from './postsSlice'
 
 
