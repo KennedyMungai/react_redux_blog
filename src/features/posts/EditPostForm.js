e4875c7b24937e4b1b108013d559f735a9a60db0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 15:05:22 +0300

    Wrote out the markup of the component

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -89,6 +89,54 @@ const EditPostForm = () =>
             setRequestStatus('idle')
         }
     }
+
+    return (
+        <section>
+            <h2> Edit Post</h2>
+            <form action="">
+                <label htmlFor="postTitle">
+                    Post Title:
+                </label>
+                <input
+                    type='text'
+                    id="postTitle"
+                    name="postTitle"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+                <label htmlFor="postAuthor">Author:</label>
+                <select
+                    id='postAuthor'
+                    value={userId}
+                    onChange={onAuthorChanged}
+                >
+                    <option value=""></option>
+                    {userOptions}
+                </select>
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    value={content}
+                    onChange={onContentChanged}
+                />
+                <button
+                    type="button"
+                    onClick={onSavePostClicked}
+                    disabled={!canSave}
+                >
+                    Save Post
+                </button>
+                <button
+                    className="deleteButton"
+                    type='button'
+                    onClick={onDeletePostClicked}
+                >
+                    Delete Post
+                </button>
+            </form>
+        </section>
+    )
 }
 
 export default EditPostForm
