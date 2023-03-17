1c1bc1e6bb6e3ac6027dcb941ce549b6a1fd8df5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 12:44:23 +0300

    Added more imports to the react component

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import PostAuthor from './PostAuthor'
 import { selectPostById } from './postsSlice'
+import ReactionButtons from './ReactionButtons'
+import TimeAgo from './TimeAgo'
 
 
 const SinglePostPage = () =>
