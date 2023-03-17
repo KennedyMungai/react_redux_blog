d5f20fc48cdce5ce7eb1d6efc04bf26b2c9b7eb4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 13:02:24 +0300

    Imported routes and route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import Layout from "./components/Layout";
+import { Route, Routes } from "react-router-dom";
 
 function App()
 {
