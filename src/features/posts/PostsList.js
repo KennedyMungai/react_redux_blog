a516c6074aef94f520a5a1e604f02d5d91b8f780
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 14:04:25 +0300

    Got rid of the Posts heading markup

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,32 +3,37 @@ import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./pos
 import { useEffect } from "react";
 import PostsExcerpt from "./PostsExcerpt";
 
-const PostsList = () => {
+const PostsList = () =>
+{
     const dispatch = useDispatch();
 
     const posts = useSelector(selectAllPosts);
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
+    useEffect(() =>
+    {
+        if (postStatus === 'idle')
+        {
             dispatch(fetchPosts())
         }
     }, [postStatus, dispatch])
 
     let content;
-    if (postStatus === 'loading') {
+    if (postStatus === 'loading')
+    {
         content = <p>"Loading..."</p>;
-    } else if (postStatus === 'succeeded') {
+    } else if (postStatus === 'succeeded')
+    {
         const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
         content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
-    } else if (postStatus === 'failed') {
+    } else if (postStatus === 'failed')
+    {
         content = <p>{error}</p>;
     }
 
     return (
         <section>
-            <h2>Posts</h2>
             {content}
         </section>
     )
